ed1184916bd7d217e851be4eb182609e5560fdb6
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2023-03-15 09:43:20 -0600

    Added PlaylistManagement System

---
+++ BlazorWebApp/PlaylistManagementSystem/BLL/PlaylistManagementService.cs
@@ -0,0 +1,49 @@
+﻿using PlaylistManagementSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistManagementSystem.BLL
+{
+    public class PlaylistManagementService
+    {
+        //  fetch playlist
+        public List<PlaylistTrackView> FetchPlaylist(string userName, string playlistName)
+        {
+            return null;
+        }
+
+        //  fetch artist or album tracks
+        public List<TrackSelectionView> FetchArtistOrAlbumTracks(string searchType,
+            string searchValue)
+        {
+            return null;
+        }
+
+        //  add track
+        public void AddTrack(string userName, string playlistName, int trackId)
+        {
+
+        }
+
+        //  remove track(s)
+        public void RemoveTracks(int playlistId, List<int> trackIds)
+        {
+
+        }
+
+        //  move tracks
+        public void MoveTracks(int playlistId, List<MoveTrackView> moveTracks)
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
