89afbc45a53878d066b2851cb205ecea96bfeb42
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2023-03-23 11:48:40 -0600

    Added Playlist Management System

--- BlazorWebApp/BlazorWebApp/Pages/SamplePages/Basics.razor.cs
+++ BlazorWebApp/BlazorWebApp/Pages/SamplePages/Basics.razor.cs
@@ -1,10 +1,27 @@
-﻿namespace BlazorWebApp.Pages.SamplePages
+﻿using Microsoft.AspNetCore.Components;
+using PlaylistManagementSystem.BLL;
+using PlaylistManagementSystem.ViewModels;
+
+namespace BlazorWebApp.Pages.SamplePages
 {
     public partial class Basics
     {
+        #region Injections
+        //  We are now injecting our service into our class using the [Inject] attribute.
+        //  Before, we would have used the page constructor to add it.
+        //  public Basic(PlaylistManagementService playlistManagementService)
+        //  {
+        //      _playlistManagementSystem = playlistManagementSystem;
+        //  }
+        [Inject]
+        protected PlaylistManagementService? PlaylistManagementService { get; set; }
+        #endregion
+
         #region Fields
         private string myName;
         private int oddEven;
+        //  working version view
+        private WorkingVersionView workingVersionView = new();
         #endregion
 
         //  Method invoked when the component is ready to start,
@@ -13,6 +30,15 @@
         {
             await base.OnInitializedAsync();
             RandomValue();
+
+        }
+
+        //  method for retrieving our version information
+        private async Task GetDatabase()
+        {
+            workingVersionView = PlaylistManagementService.GetWorkingVersion();
+            //  wait for the data to be retreived before we update the label on the page
+            await InvokeAsync(StateHasChanged);
         }
 
         private void RandomValue()
@@ -28,6 +54,6 @@
                 myName = null;
             }
         }
-       
+
     }
 }
--- BlazorWebApp/BlazorWebApp/Pages/SamplePages/PlaylistManagement.razor.cs
+++ BlazorWebApp/BlazorWebApp/Pages/SamplePages/PlaylistManagement.razor.cs
@@ -1,6 +1,55 @@
-﻿namespace BlazorWebApp.Pages.SamplePages
+﻿
+using Microsoft.AspNetCore.Components;
+using PlaylistManagementSystem.BLL;
+using PlaylistManagementSystem.ViewModels;
+
+
+namespace BlazorWebApp.Pages.SamplePages
 {
     public partial class PlaylistManagement
     {
+        #region Inject
+        // we are injecting our service into our class using the [Inject] attribute
+        [Inject]
+        protected PlaylistManagementService? PlaylistManagementService { get; set; }
+        #endregion
+
+        #region fields
+
+        private string searchPattern { get; set; } = "Deep";
+        // preset the search type so that the radio button has a default of "Artist"
+        private string searchType { get; set; } = "Artist";
+        private string playlistName { get; set; } = "HansenB1";
+        private string userName { get; set; } = "HansenB";
+        private int playlistId { get; set; }
+        private string feedback { get; set; }
+        #endregion
+
+        protected List<PlaylistTrackView> Playlists { get; set; } = new();
+
+        #region Paginator
+        // Desired current page size
+        private const int PAGE_SIZE = 10;
+
+        // sort column used with the paginator
+        protected string SortField { get; set; } = "Owner";
+        
+        // sort direction for the paginator
+        protected string Direction { get; set; } = "desc";
+
+        //  current page for the paginator
+        protected int CurrentPage { get; set; } = 1;
+
+        //paginator collection of tracks selection view
+        protected PageResult<TrackSelectionView> PaginatorTrackSelection { get; set; } = new();
+
+        #endregion
+
+
+        private async Task FetchArtistOrAlbumTracks()
+        {
+            //  we would normal check if the user has enter in a value into the search
+            //      pattern but we will let the service do the error checking
+        }
     }
 }
